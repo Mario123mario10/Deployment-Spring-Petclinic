551966890d95a8d600655d5e9b84e8cb7bb4dd69
mpakulsk <01169219@pw.edu.pl>
2023-11-20 19:47:16 +0100

    Upload New File

---
+++ nginx.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Aktualizacja systemu i instalacja NGINX
+sudo apt update
+sudo apt install -y nginx
+
+# Zatrzymywanie serwera NGINX na czas konfiguracji
+sudo systemctl stop nginx
+
+# Tworzenie nowego pliku konfiguracyjnego dla NGINX
+cat <<EOT | sudo tee /etc/nginx/nginx.conf
+http {
+    upstream backend_servers {
+        server 10.0.0.5:8080;
+        server 10.0.0.8:8080;
+    }
+
+    server {
+        listen 80;
+
+        location / {
+            proxy_pass http://backend_servers;
+            proxy_set_header Host \$host;
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto \$scheme;
+        }
+    }
+}
+EOT
+
+# Restart serwera NGINX aby zastosować nową konfigurację
+sudo systemctl start nginx
