13884ca2577cb8ceb13db3b6bf5451107b98a154
mpakulsk <01169219@pw.edu.pl>
2023-11-22 15:50:21 +0100

    Update file nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -34,33 +34,3 @@ sudo systemctl start nginx
 
 
 
-
-http {
-    upstream backend-master {
-        server master-backend-ip:port;
-    }
-
-    upstream backend-slave {
-        server slave-backend-ip:port;
-    }
-
-    server {
-        listen 80;
-
-        location / {
-            if ($request_method = GET) {
-                proxy_pass http://backend-slave;
-            }
-
-            if ($request_method ~* "(POST|PUT|DELETE)") {
-                proxy_pass http://backend-master;
-            }
-
-            # Common proxy settings
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-        }
-    }
-}
