a0b204e5d593016340e683d64b9756422e187074
bkisly <b.kisly@gmail.com>
2023-12-13 17:07:12 +0100

    Fixed frontend

--- Lab2/conf_2/src/frontend/frontend.sh
+++ Lab2/conf_2/src/frontend/frontend.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
 
-FRONT_PORT=$1
-API_IP=$2
-API_PORT=$3
-
-apt-get update -y
-apt-get upgrade -y
-apt-get -y install curl git
-
-curl -sL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
-bash /tmp/nodesource_setup.sh
-apt-get -y install nodejs
+FRONTEND_PORT="$1"
+BACKEND_IP="$2"
+BACKEND_PORT="$3"
 
 mkdir -p /petclinic/frontend
 cd /petclinic/frontend
 
+apt update
+apt upgrade -y
+apt install npm -y
+
+npm cache clean -f
+npm install -g n
+n stable
+hash -r
+
+npm uninstall -g angular-cli @angular/cli
+npm install -g @angular/cli@latest
+
 git clone https://github.com/spring-petclinic/spring-petclinic-angular.git
 cd spring-petclinic-angular/
+sed -i "s/localhost/$BACKEND_IP/g" src/environments/environment.ts src/environments/environment.prod.ts
+sed -i "s/9966/$BACKEND_PORT/g" src/environments/environment.ts src/environments/environment.prod.ts
 
+npm install --save-dev @angular/cli@latest --force
 rm package-lock.json
-npm install --dev @angular/cli@latest -y
-# npm install --save-dev @angular/cli@latest --force
 npm install --force
 
-sed -i "s/localhost/$API_IP/g" src/environments/environment.ts src/environments/environment.prod.ts
-sed -i "s/9966/$API_PORT/g" src/environments/environment.ts src/environments/environment.prod.ts
-
-source <(ng completion script)
-ng analytics off
-
-npx ng build
-npm install angular-http-server --force
-npx angular-http-server --path ./dist -p "$FRONT_PORT" &
+echo N | ng analytics off
+echo Y | ng serve --host 0.0.0.0 --port $FRONTEND_PORT &
