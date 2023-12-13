dbc743dfdc7af386401819f7b7a0a19f2da63574
bkisly <b.kisly@gmail.com>
2023-12-13 16:35:34 +0100

    Changes to script organization

--- Lab2/conf_2/src/backend/backend.sh
+++ Lab2/conf_2/src/backend/backend.sh
@@ -7,7 +7,7 @@ DB_PORT_MASTER=$3
 
 apt-get -y update
 apt-get -y upgrade
-apt-get -y install default-jdk git mariadb-client
+apt-get -y install openjdk-17-jdk git mariadb-client
 
 mkdir -p /petclinic/backend_write
 cd /petclinic/backend_write
@@ -18,7 +18,7 @@ cd ./spring-petclinic-rest
 master_db_url="jdbc:mysql:\/\/${DB_IP_MASTER}:${DB_PORT_MASTER}\/petclinic?useUnicode=true&allowSourceDownConnections=true"
 
 sed -i "s/hsqldb,//g" src/main/resources/application.properties src/test/resources/application.properties
-sed -i "s/9966/3000/g" src/main/resources/application.properties
+sed -i "s/9966/${API_PORT_WRITE}/g" src/main/resources/application.properties
 
 sed -i "38s/^/spring.datasource.url = ${master_db_url}\n/" src/main/resources/application.properties
 sed -i "39s/^/spring.datasource.username=pc\n/" src/main/resources/application.properties
@@ -30,4 +30,5 @@ sed -i "44s/^/spring.jpa.hibernate.ddl-auto=none\n/" src/main/resources/applicat
 
 while [ "$(mysqladmin ping -u pc --password=petclinic  -h ${DB_IP_MASTER} -P ${DB_PORT_MASTER})" != "mysqld is alive" ]; do sleep 2; done; 
 
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/
 ./mvnw spring-boot:run &
